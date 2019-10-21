9538a03bcc35ff933958acd17f8e96679c053c46
Edvard DHG19 <56823476+Dvard@users.noreply.github.com>
2019-10-21 13:39:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const text = 'Kalle tycker mycket om att koda';
+console.log(text.length);
+console.log(text.indexOf('koda'));
+console.log(text.slice('mycket'));
+console.log(text.replace('mycket', 'inte'));
+console.log(text.toUpperCase());
+
+const num1 = 9;
+console.log(Math.pow(num1, 2));
+console.log(num1.toFixed(2));
+
+let str_num = '9';
+str_num = parseInt(str_num);
